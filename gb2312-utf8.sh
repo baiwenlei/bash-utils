2084ab6dff65080025bd93cd5a424f89f474bfe0
白文磊 <baiwenlei@gmail.com>
2015-09-03 10:32:19 +0800

    优化: 编码转换支持批量指定扩展名

--- gb2312-utf8.sh
+++ gb2312-utf8.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-files=`find . -name *.java`
-for file in $files
+types=$@
+for type in $types
 do
-    printf "convert $file ... "
-    dir=`dirname $file`
-    iconv -f GB2312 -t UTF-8 $file > $dir/temp.java
-    if [ !$? ]; then
-        mv $dir/temp.java $file
-        printf "done\n"
-    else
-        rm -f $dir/temp.java
-    fi
+    files=`find . -name *.$type`
+    for file in $files
+    do
+        printf "convert $file ... "
+        dir=`dirname $file`
+        iconv -f GB2312 -t UTF-8 $file > ${file}.iconv
+        if [ !$? ]; then
+            mv ${file}.iconv $file
+            printf "done\n"
+        else
+            rm -f ${file}.iconv
+        fi
+    done
 done
